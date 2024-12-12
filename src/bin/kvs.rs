e0325dbbef175fdc6224c0a679bf1b622a2c7dc4
ayhonz <janmachacek97@gmail.com>
2024-12-12 11:46:44 +0100

    create cli with clap and basics of kvsStore

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,36 @@
+use std::process;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Get { key: String },
+    Set { key: String, value: String },
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello world")
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Get { key } => {
+            eprintln!("unimplemented");
+            process::exit(1)
+        }
+        Commands::Set { key, value } => {
+            eprintln!("unimplemented");
+            process::exit(1)
+        }
+        Commands::Rm { key } => {
+            eprintln!("unimplemented");
+            process::exit(1)
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,41 @@
-use core::panic;
+#![deny(missing_docs)]
+//! A key value store that enables persistant storage
+use std::collections::HashMap;
 
-pub struct KvStore {}
+/// KvStore stores key/value string pairs.
+/// Key/value pair is stored in hashMap
+///```rust
+///# use kvs::KvStore;
+///let mut store = KvStore::new();
+///store.set("key1".to_string(), "value1".to_string());
+///let value = store.get("key1".to_string());
+///assert_eq!(value, Some("value1".to_string()));
+///```
+#[derive(Default)]
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
 
 impl KvStore {
+    /// creates a new instance of KvStore
     pub fn new() -> Self {
-        panic!()
+        KvStore {
+            store: HashMap::new(),
+        }
     }
 
-    pub fn set(&self, key: String, value: String) -> Option<String> {
-        panic!()
+    /// Store key-value pair, does not return anything for now
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
     }
 
+    /// get value by key, returns Some if value exists else None
     pub fn get(&self, key: String) -> Option<String> {
-        panic!()
+        self.store.get(&key).map(|value| value.to_string())
     }
 
-    pub fn remove(&self, key: String) -> Option<String> {
-        panic!()
+    /// Removes value from store
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
     }
 }
